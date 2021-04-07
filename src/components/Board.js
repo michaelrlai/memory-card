1c434f56f76630e24306cfdf5f86633d719fd31e
Michael Lai <mrlai7782@gmail.com>
2021-04-06 18:56:36 -0700

    update

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from "react";
 import bulbasaur from "../images/bulbasaur.png";
 import caterpie from "../images/caterpie.png";
 import clefairy from "../images/clefairy.png";
@@ -15,19 +16,85 @@ import vulpix from "../images/vulpix.png";
 import weedle from "../images/weedle.png";
 
 const Board = () => {
+  const images = [
+    {
+      src: bulbasaur,
+      title: "Bulbusaur",
+    },
+    {
+      src: caterpie,
+      title: "Caterpie",
+    },
+    {
+      src: clefairy,
+      title: "Clefair",
+    },
+    {
+      src: ekans,
+      title: "Ekans",
+    },
+    {
+      src: jigglypuff,
+      title: "Jigglypuff",
+    },
+    {
+      src: nidoran,
+      title: "Nidoran",
+    },
+    {
+      src: nidorana,
+      title: "Nidorana",
+    },
+    {
+      src: pidgey,
+      title: "Pidgey",
+    },
+    {
+      src: pikachu,
+      title: "Pikachu",
+    },
+    {
+      src: rattata,
+      title: "Rattata",
+    },
+    {
+      src: sandshrew,
+      title: "Sandshrew",
+    },
+    {
+      src: spearow,
+      title: "Spearow",
+    },
+    {
+      src: squirtle,
+      title: "Squirtle",
+    },
+    {
+      src: vulpix,
+      title: "Vulpix",
+    },
+    {
+      src: weedle,
+      title: "Weedle",
+    },
+  ];
+
+  const [cards, setCards] = useState(images);
+
+  const handleClick = () => {
+    console.log(cards);
+  };
+
   return (
     <div className="board">
+      <button onClick={handleClick}>Button</button>
       <div className="card-container">
-        <div className="card">
-          <img src={bulbasaur} alt="" />
-        </div>
-        <div className="card">card</div>
-        <div className="card">card</div>
-        <div className="card">card</div>
-        <div className="card">card</div>
-        <div className="card">card</div>
-        <div className="card">card</div>
-        <div className="card">card</div>
+        {cards.map((card) => (
+          <button key={card.title} className="card">
+            <figure>{card.title}</figure>
+            <img src={card.src} alt="" />
+          </button>
+        ))}
       </div>
     </div>
   );
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,7 +1,11 @@
+import React, { useState } from "react";
 import Header from "./Header";
 import Board from "./Board";
 
 const Game = () => {
+  const [score, setScore] = useState(0);
+  const [highScore, setHighScore] = useState(0);
+
   return (
     <div>
       <Header />
